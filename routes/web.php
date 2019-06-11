3a59ff51b6a0f22a8613615eeb2f63ce3d1d03b3
Aung Myo Khaing <aungmyokhaing.arata@gmail.com>
2019-06-11 16:45:42 +0630

    first commit

---
+++ routes/web.php
@@ -0,0 +1,36 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function() {
+
+    $crawler = Goutte::request('GET', 'https://www.jobsinyangon.com/app/job-search?send=1&reset_filtr=1');
+
+    $data = $crawler->filter('.accordion-toggle')->each(function ($node) {
+
+        $info = explode(',', ($node->filter('.seda-t')) ? trim($node->filter('.seda-t')->text()) : '');
+
+        $test = preg_replace("/[^a-zA-Z0-9\s]/", "", $info[1]);
+        return array(
+            'title' => $node->filter('.nazev-inzeratu-vypis')->text(),
+            'category' => $node->filter('p span')->text(),
+            'company' => $node->filter('.col-lg-3 a')->first()->text(),
+            'location' => $node->filter('.col-lg-3 a')->last()->text(),
+            'posted' => $node->filter('.seda-t span')->text(),
+            'employment_type' => !empty($info) ? trim($info[1]) : '',
+            'job_field' => !empty($info) ? $info[2] : '',
+            'detail_link' => $node->filter('.plna-sirka-na-mobilu')->attr('href'),
+        );
+    });
+
+    dd($data);
+});
